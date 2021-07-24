26e10eaf79c254cf2b2f842e265cafbfa8552615
luancardosoti <luancardosoti@gmail.com>
2021-07-24 17:26:15 -0300

    :sparkles: register a todo and list todos the user if user exists

--- src/index.js
+++ src/index.js
@@ -12,6 +12,16 @@ const users = [];
 
 function checksExistsUserAccount(request, response, next) {
   // Complete aqui
+  const { username } = request.headers;
+  const user = users.find((user) => user.username === username);
+
+  if (!user) {
+    return response.status(404).json({ error: "User not found !" });
+  }
+
+  request.user = user;
+
+  next();
 }
 
 app.post("/users", (request, response) => {
@@ -36,11 +46,26 @@ app.post("/users", (request, response) => {
 });
 
 app.get("/todos", checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+
+  return response.json(user.todos);
 });
 
 app.post("/todos", checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { title, deadline } = request.body;
+  const { user } = request;
+
+  const todo = {
+    id: uuidv4(),
+    title,
+    done: false,
+    deadline: new Date(deadline),
+    created_at: new Date(),
+  };
+
+  user.todos.push(todo);
+
+  return response.status(201).json(todo);
 });
 
 app.put("/todos/:id", checksExistsUserAccount, (request, response) => {
